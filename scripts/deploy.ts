83f8e658e526b1c17327f6a73a381e6619e6a23f
REGO350 <riokanehiro228pa@gmail.com>
2021-11-28 13:03:38 +0900

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,9 @@
-import { LimiSwap__factory, MockERC20__factory } from "../typechain";
-import { MacroChain, toWei, verifyContract } from "../utils"
+import { LimiSwap__factory } from "../typechain";
+import { MacroChain, verifyContract } from "../utils"
 
 const main = async () => {
   const { deployer } = await MacroChain.init();
 
-  //Deploy mock tokens
-  // const supply = toWei(1000000);
-  // await deployer<MockERC20__factory>("MockERC20", ["a", "A", supply], true);
-  // await deployer<MockERC20__factory>("MockERC20", ["b", "B", supply], true);
-
-  //Deploy LimiSwap contract
   const keeper = "0x4Cb093f226983713164A62138C3F718A5b595F73";
   const router = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
   const quoter = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
